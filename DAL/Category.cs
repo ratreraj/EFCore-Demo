1d53cb6d1a8fe4f9cdf9e6bcd7c3c5ce43190645
ratre <ratre@DESKTOP-HA42MEA>
2021-05-22 00:24:55 +0530

    Add project files.

---
+++ DAL/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        //Navigation Property, help full in lazy loading
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ DAL/DataBaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext()
+        { }
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+            // var t = Categories;
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"data source=DESKTOP-HA42MEA\SQLEXPRESS; initial catalog=EFCore;persist security info=True;user id=sa;password=Sql@1234; MultipleActiveResultSets=True; App=EntityFramework");
+                //  optionsBuilder.UseSqlServer(@"Data Source=KAPIL\\SQLEXPRESS;Initial Catalog=testDB;Integrated Security=true");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
